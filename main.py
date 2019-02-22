191728e8fb9448f36c9abde635254cb7ed5097c4
Pedro Javier Bohorquez Camelo <000405199@bga.upb>
2019-02-22 13:45:04 -0500

    cuarto commit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-print("segundo commit")
 
+import math
 a=int(input("digite un numero"))
-c=5
+b=int(input("digie un numero"))
 
+c=5
 x=a*c
 y=2*a
+z=b**2
 
 print("el producto de ",a," y ",c, " es ",x)
 print("el doble producto de ",a, " es ",y)
-
-print("Listo")
+print("el cuadrado del numero ",b," es ",z)
+print("la raiz cuadrada del numero ",b, " es " + str(math.sqrt(b)))
