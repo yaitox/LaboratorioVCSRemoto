501a1aa65906ba111579721b92a7863625e7c994
Pedro Javier Bohorquez Camelo <000405199@bga.upb>
2019-02-22 14:04:11 -0500

    quinto commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 a=int(input("digite un numero"))
 b=int(input("digie un numero"))
 
+
 c=5
 x=a*c
 y=2*a
@@ -11,4 +12,12 @@ z=b**2
 print("el producto de ",a," y ",c, " es ",x)
 print("el doble producto de ",a, " es ",y)
 print("el cuadrado del numero ",b," es ",z)
-print("la raiz cuadrada del numero ",b, " es " + str(math.sqrt(b)))
+print("la raiz cuadrada del numero ",d, " es " + str(math.sqrt(b)))
+
+print ("hallaremos la solución de la de la fórmula cuadrática con los datos ",a, " , ",b," , ",c)
+
+formCuadrat=(-b+str(math.sqrt((b**2)-4*a*c)))/2*a
+formCuadratNeg=(-b-str(math.sqrt((b**2)-4*a*c)))/2*a
+
+
+
